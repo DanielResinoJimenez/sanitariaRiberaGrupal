5658e8859cea0e8d9268fb129b689533cca20476
Daniel Resino <daniel.resinojimenez@riberadeltajo.es>
2025-02-04 16:41:57 +0100

    Model changes

--- sanitariaApi/src/database/models/Muestra.js
+++ sanitariaApi/src/database/models/Muestra.js
@@ -1,25 +1,34 @@
-const sequelize = require("sequelize");
+const sequelize = require("../db");
 
-const { Model, DataTypes } = require("../db");
+const { Model, DataTypes } = require("sequelize");
 
 class User extends Model {}
 
 Model.init({
     id_muestra: {
-        
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER
     },
     fecha_muestra: {
+        type: DataTypes.DATE,
+        allowNull: false,
 
     },
     observaciones: {
-
+        type: DataTypes.TEXT,
+        allowNull: false,
     },
     descripcion: {
-
+        type: DataTypes.TEXT,
+        allowNull: false,
     },
     tincion: {
-
+        type: DataTypes.STRING,
+        allowNull: false,
     }
 },{
-
+    sequelize,
+    modelName: "tincion",
+    timestamps: false
 })
