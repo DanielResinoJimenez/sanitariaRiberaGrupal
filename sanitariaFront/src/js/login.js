9d6e6c5b964ce2c9f8300d46aa8460bff8d6f309
JLVazquez22 <vazaceitunojoseluis@gmail.com>
2025-02-06 19:58:36 +0100

    Add JS in the login and register

--- sanitariaFront/src/js/login.js
+++ sanitariaFront/src/js/login.js
@@ -2,6 +2,8 @@ const loginForm = document.getElementById('loginForm');
 const registerForm = document.getElementById('registerForm');
 const loginLink = document.getElementById('loginLink');
 const registerLink = document.getElementById('registerLink');
+const passwordForm = document.getElementById('passwordForm');
+const getPassword = document.getElementById('getPassword');
 
 // Show login or register form
 const showForm = (event) => {
@@ -16,5 +18,15 @@ const showForm = (event) => {
     }
 }
 
+// Show restart password form
+const showRestartForm = (event) => {
+    event.preventDefault();
+    loginForm.classList.add("hiddenForm");
+    registerForm.classList.add("hiddenForm");
+    passwordForm.classList.remove("hiddenForm");
+}
+
+// Event listeners
 registerLink.addEventListener("click", showForm);
 loginLink.addEventListener("click", showForm);
+getPassword.addEventListener("click", showRestartForm);
