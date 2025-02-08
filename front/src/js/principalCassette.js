0b4d3c4a6864ee62344ba5f5545b7061ae82412f
TaniaRobledo <tania.robledoabenojar@riberadeltajo.es>
2025-02-08 18:37:41 +0100

    organs filtered action

--- front/src/js/principalCassette.js
+++ front/src/js/principalCassette.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', () => {
+
   const btnNuevo = document.getElementById('btnNuevo');
   const btnBorrarDetalle = document.getElementById('btnBorrarDetalle');
   const btnModificarDetalle = document.getElementById('btnModificarDetalle');
@@ -23,15 +23,87 @@ document.addEventListener('DOMContentLoaded', () => {
   const modalModificarClose = document.getElementById('modalModificarClose');
   const modalConfirmarBorrarClose = document.getElementById('modalConfirmarBorrarClose');
   const modalConfirmarBorrarEliminar = document.getElementById('modalConfirmarBorrarEliminar');
+  const organos = document.getElementById('organos');
 
   const descripcion = document.getElementById('cassette__descripcion');
   const organo = document.getElementById('cassette__organo');
   const caracteristicas = document.getElementById('cassette__caracteristicas');
   const observaciones = document.getElementById('cassette__observaciones');
 
-  //Guardar los cassettes en el localStorage
-  let cassettes = JSON.parse(localStorage.getItem('cassettes')) || [];
+  
+  // Modal Nueva Muestra
+  const btnNuevaMuestra = document.getElementById('btnNuevaMuestra');
+  const modalNuevaMuestra = document.getElementById('modalNuevaMuestra');
+  const modalMuestraSave = document.getElementById('modalMuestraSave');
+  const modalMuestraClose = document.getElementById('modalMuestraClose');
+
+
+  let cassettes = [];
   let selectedIndex = -1;
+
+  const actualizarTabla = (cassetes_to_show) => {
+    const tbody = document.querySelector('section:nth-child(1) tbody');
+    tbody.innerHTML = '';
+    cassetes_to_show.forEach((cassette, index) => {
+      const row = document.createElement('tr');
+      row.innerHTML = `
+        <td class="px-4 py-2">${cassette.fecha}</td>
+        <td class="px-4 py-2">${cassette.descripcion}</td>
+        <td class="px-4 py-2">${cassette.organo}</td>
+      `;
+      row.addEventListener('click', () => {
+        mostrarDetalles(index);
+      });
+      tbody.appendChild(row);
+    });
+  }
+
+  const mostrarDetalles = (index) => {
+    const cassette = cassettes[index];
+    descripcion.textContent = cassette.descripcion;
+    organo.textContent = cassette.organo;
+    caracteristicas.textContent = cassette.caracteristicas;
+    observaciones.textContent = cassette.observaciones;
+    selectedIndex = index;
+    actualizarTablaMuestras();
+  }
+
+  const limpiarDetalles = () => {
+    descripcion.textContent = '---';
+    organo.textContent = '---';
+    caracteristicas.textContent = '---';
+    observaciones.textContent = '---';
+    selectedIndex = -1;
+  }
+
+  // Función para actualizar la tabla de muestras
+  const actualizarTablaMuestras  = () => {
+    const tbody = document.querySelector('section:nth-child(2) tbody');
+    tbody.innerHTML = '';
+    if (selectedIndex >= 0 && selectedIndex < cassettes.length) {
+      const cassette = cassettes[selectedIndex];
+      if (cassette.muestras) {
+        cassette.muestras.forEach((muestra, index) => {
+          const row = document.createElement('tr');
+          row.innerHTML = `
+            <td class="px-4 py-2">${muestra.fecha}</td>
+            <td class="px-4 py-2">${muestra.descripcion}</td>
+            <td class="px-4 py-2">${muestra.tincion}</td>
+            <td class="px-4 py-2"><button class="bg-blue-500 text-white px-2 py-1 rounded hover:bg-blue-600" onclick="verDetallesMuestra(${index})">Detalles</button></td>
+          `;
+          tbody.appendChild(row);
+        });
+      }
+    }
+  }
+
+  // Función para ver detalles de una muestra
+const verDetallesMuestra = (index) => {
+  const cassette = cassettes[selectedIndex];
+  const muestra = cassette.muestras[index];
+  // alert(`Detalles de la muestra:\nFecha: ${muestra.fecha}\nDescripción: ${muestra.descripcion}\nTinción: ${muestra.tincion}\nObservaciones: ${muestra.observaciones}`);
+}
+
 
   btnNuevo.addEventListener('click', () => {
     selectedIndex = -1;
@@ -134,67 +206,7 @@ document.addEventListener('DOMContentLoaded', () => {
     document.body.classList.remove('modal-open');
   });
 
-  function actualizarTabla() {
-    const tbody = document.querySelector('section:nth-child(1) tbody');
-    tbody.innerHTML = '';
-    cassettes.forEach((cassette, index) => {
-      const row = document.createElement('tr');
-      row.innerHTML = `
-        <td class="px-4 py-2">${cassette.fecha}</td>
-        <td class="px-4 py-2">${cassette.descripcion}</td>
-        <td class="px-4 py-2">${cassette.organo}</td>
-      `;
-      row.addEventListener('click', () => {
-        mostrarDetalles(index);
-      });
-      tbody.appendChild(row);
-    });
-  }
 
-  function mostrarDetalles(index) {
-    const cassette = cassettes[index];
-    descripcion.textContent = cassette.descripcion;
-    organo.textContent = cassette.organo;
-    caracteristicas.textContent = cassette.caracteristicas;
-    observaciones.textContent = cassette.observaciones;
-    selectedIndex = index;
-    actualizarTablaMuestras();
-  }
-
-  function limpiarDetalles() {
-    descripcion.textContent = '---';
-    organo.textContent = '---';
-    caracteristicas.textContent = '---';
-    observaciones.textContent = '---';
-    selectedIndex = -1;
-  }
-
-  // Función para actualizar la tabla de muestras
-  function actualizarTablaMuestras() {
-    const tbody = document.querySelector('section:nth-child(2) tbody');
-    tbody.innerHTML = '';
-    if (selectedIndex >= 0 && selectedIndex < cassettes.length) {
-      const cassette = cassettes[selectedIndex];
-      if (cassette.muestras) {
-        cassette.muestras.forEach((muestra, index) => {
-          const row = document.createElement('tr');
-          row.innerHTML = `
-            <td class="px-4 py-2">${muestra.fecha}</td>
-            <td class="px-4 py-2">${muestra.descripcion}</td>
-            <td class="px-4 py-2">${muestra.tincion}</td>
-            <td class="px-4 py-2">${muestra.tipo}</td>
-          `;
-          tbody.appendChild(row);
-        });
-      }
-    }
-  }
-
-  // Modal Nueva Muestra
-  const btnNuevaMuestra = document.getElementById('btnNuevaMuestra');
-  const modalNuevaMuestra = document.getElementById('modalNuevaMuestra');
-  const modalMuestraSave = document.getElementById('modalMuestraSave');
-  const modalMuestraClose = document.getElementById('modalMuestraClose');
 
   btnNuevaMuestra.addEventListener('click', () => {
     if (selectedIndex >= 0 && selectedIndex < cassettes.length) {
@@ -232,11 +244,29 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
 
-  // Función para que al crear un nuevo cassette la fecha no pueda ser anterior al día de hoy
+  organos.addEventListener('change', () => {
+    const organo = organos.value;
+    if (organo === '*') {
+      actualizarTabla(cassettes);
+      return;
+    }
+    const filtered_cassettes = cassettes.filter(cassette => cassette.organo === organo);
+    actualizarTabla(filtered_cassettes);
+  });
+
+
+  document.addEventListener('DOMContentLoaded', () => {
+    //Guardar los cassettes en el localStorage
+  cassettes = JSON.parse(localStorage.getItem('cassettes')) || [];
+  selectedIndex = -1;
+
+      // Función para que al crear un nuevo cassette la fecha no pueda ser anterior al día de hoy
   const fechaInput = document.getElementById('modalFecha');
   const hoy = new Date().toISOString().split('T')[0];
   fechaInput.setAttribute('min', hoy);
 
   // Mostrar cassettes inicialmente
-  actualizarTabla();
-});
+  actualizarTabla(cassettes);
+  
+  });
+
